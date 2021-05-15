f2c252c6689591b28aa5ab3e2170161d0cea09cd
Suraj Ajay Dwivedi <66689016+Surajajaydwivedi@users.noreply.github.com>
2021-05-15 15:54:12 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,7 +6,29 @@ import requests
 
 URL = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByPin?pincode="
 age = -1
+'''# Importing necessary modules
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+import ast
 
+# WebDriver Chrome
+driver = webdriver.Chrome(ChromeDriverManager().install())
+
+# Target URL
+driver.get("https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByPin?pincode=281001&date=08/05/2021")
+
+# print(driver.title)
+
+# Printing the whole body text
+s=(driver.find_element_by_xpath("/html/body").text)
+driver.close()
+s=s[12:-1]
+s=ast.literal_eval(s)
+print("Vaccines Available!!")
+print("details : ")
+for i in s:
+    print(i["name"],"(",i["min_age_limit"],"+) : ",i["available_capacity"])
+# Closing the driver'''
 def check_inf(pincode):
 	starttime = time.time()
 	while True:
